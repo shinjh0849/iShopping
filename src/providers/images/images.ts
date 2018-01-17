68a5a4524f1528c5d3c6afc4adc48d11f839bcb4
shinjh0849 <21200421@handong.edu>
2018-01-17 10:55:34 +0900

    server connection complete!

--- src/providers/images/images.ts
+++ src/providers/images/images.ts
@@ -11,7 +11,7 @@ import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-nati
 */
 @Injectable()
 export class ImagesProvider {
-  apiURL = 'ec2-13-124-2-58.ap-northeast-2.compute.amazonaws.com/:80';
+  apiURL = 'http://ec2-13-124-2-58.ap-northeast-2.compute.amazonaws.com/';
 
   constructor(public http: Http, private transfer: FileTransfer) {
     console.log('Hello ImagesProvider Provider');
